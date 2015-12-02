188da9e0e6cded02215f7d27e7883208eac45407
MATSUMOTO Ryosuke <matsumoto_r@net.ist.i.kyoto-u.ac.jp>
2015-12-02 19:47:44 +0900

    Cosmetic change

--- mrblib/rcon.rb
+++ mrblib/rcon.rb
@@ -1,5 +1,16 @@
 def __main__(argv)
-  opts = Getopts.getopts("", "cpu:30", "memory:#{512 * 1024 * 1024}", "read:#{10 * 1024 * 1024}", "write:#{10 * 1024 * 1024}", "group:rcon", "user:", "command:", "dev:8:0", "version", "help", "dry-run").each_with_object({}) {|ary,opts| opts[ary[0].to_sym] = ary[1] }
+  opts = Getopts.getopts("",
+                         "cpu:30",
+                         "memory:#{512 * 1024 * 1024}",
+                         "read:#{10 * 1024 * 1024}",
+                         "write:#{10 * 1024 * 1024}",
+                         "group:rcon",
+                         "user:",
+                         "command:",
+                         "dev:8:0",
+                         "version",
+                         "help",
+                         "dry-run").each_with_object({}) {|ary,opts| opts[ary[0].to_sym] = ary[1] }
 
   raise ArgumentError, "\n\n#{Rconner::USAGE}\n" if opts.has_key?(:"?")
 
@@ -22,22 +33,17 @@ def __main__(argv)
   raise ArgumentError, "user not found: --user" if opts[:user].empty?
 
   Rcon.new({
-    :command => opts[:command],
-    :user => opts[:user],
+    :command  => opts[:command],
+    :user     => opts[:user],
     :resource => {
-      # centos "/cgroup" by default
-      # ubuntu "/sys/fs/cgroup"
-      :root => "/cgroup",
-      :group => opts[:group],
-      # CPU [msec] exc: 30000 -> 30%
-      :cpu_quota => opts[:cpu].to_i * 1000,
-      # IO [Bytes/sec]
-      :blk_dvnd => opts[:dev],
-      :blk_rbps => opts[:read].to_i,
-      :blk_wbps => opts[:write].to_i,
-      # Memory [Bytes]
-      :mem => opts[:memory].to_f,
-      :oom => true,
+      :root       => "/cgroup",
+      :group      => opts[:group],
+      :cpu_quota  => opts[:cpu].to_i * 1000,
+      :blk_dvnd   => opts[:dev],
+      :blk_rbps   => opts[:read].to_i,
+      :blk_wbps   => opts[:write].to_i,
+      :mem        => opts[:memory].to_f,
+      :oom        => true,
     },
   }).run
   opts
